e99eb8a80e6fb5a822b1194f410eabf1b1cfe09f
João  Lima <joao.lima@somoseducacao.com.br>
2023-02-15 20:01:11 -0300

    fix tests

--- src/application/use-cases/create-cards-use-case.ts
+++ src/application/use-cases/create-cards-use-case.ts
@@ -7,16 +7,16 @@ export class CreateCardsUseCase {
   ){}
   async execute(cards: Card[], setId: string){
     try{
-      
       await this.cardsRepository.createCards(cards.map(card => {
         card.setId = setId
-        card.faces = card.faces.map(face => {
+        card.faces = card.faces?.map(face => {
           face.setId = setId
           return face
         })
         return card
       }))
     }catch(error){
+      console.error(error)
       throw new Error(`Erro ao tentar cadastrar as cartas`)
     }
   }
